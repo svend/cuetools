2371a4f51aa9ad31b78a9559eeb8b3b33c220c87
Svend Sorensen <svend@ciffer.net>
2005-03-18 22:46:12 +0000

    Add comments about INDEX pregap vs. PREGAP.

--- tools/cuebreakpoints.c
+++ tools/cuebreakpoints.c
@@ -45,7 +45,8 @@ void print_breaks (Cd *cd)
 	long b;
 	Track *track;
 
-	for (i = 1; i <= cd_get_ntrack(cd); i++) {
+	/* start on track 2 */
+	for (i = 2; i <= cd_get_ntrack(cd); i++) {
 		track = cd_get_track(cd, i);
 		/* don't print zero indexes */
 		b = track_get_start(track) + track_get_index(track, 1) -  track_get_zero_pre(track);
