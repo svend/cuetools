0d6072e1aad16cb0f0f44ec6936c87cd6060b5b7
Svend Sorensen <svend@ciffer.net>
2005-03-22 04:37:07 +0000

    Removed lib and tools dirs (again?).

--- lib/toc.h
+++
@@ -1,9 +0,0 @@
-/*
- * toc.h -- toc function declarations
- *
- * Copyright (C) 2004 Svend Sorensen
- * For license terms, see the file COPYING in this distribution.
- */
-
-Cd *toc_parse (FILE *fp);
-void toc_print (FILE *fp, Cd *cd);
