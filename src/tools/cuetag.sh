3949cccc457b60f90be828f18edfb7beca313219
Dmitry Smirnov <onlyjob@member.fsf.org>
2013-07-05 14:54:32 +1000

    fix cli parameters for metaflac

--- src/tools/cuetag.sh
+++ src/tools/cuetag.sh
@@ -29,7 +29,7 @@ vorbis()
 {
 	# FLAC tagging
 	# --remove-vc-all overwrites existing comments
-	METAFLAC="metaflac --remove-vc-all --import-vc-from=-"
+	METAFLAC="metaflac --remove-all-tags --import-tags-from=-"
 
 	# Ogg Vorbis tagging
 	# -w overwrites existing comments
